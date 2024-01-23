62e044c7312033d41d63b1925247a7c7e0072822
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-01-23 18:31:31 +0300

    Style Logo component and add aria-label to it

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import '../styles/logo.sass';
 
 export default function Logo() {
   return (
-    <Link to="/" className="logo">
-      Shopping Cart
+    <Link to="/" className="logo" aria-label="Go to home page">
+      Logo
     </Link>
   );
 }
