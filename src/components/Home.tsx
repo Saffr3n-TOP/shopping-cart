cc794e82857866f03fef8eae1be98cd41d433a15
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-01-23 20:23:52 +0300

    Create Home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="heading">Home Page</h1>
+
+      <p className="paragraph">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae,
+        autem quia. Nam aperiam suscipit ea exercitationem itaque sequi porro
+        iste perferendis dignissimos quod doloremque, possimus fugiat
+        perspiciatis numquam, dolore est?
+      </p>
+
+      <p className="paragraph">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, minima
+        molestias veniam enim hic porro consectetur aliquid numquam, officiis ea
+        at neque aperiam. Possimus iure et veritatis? Porro, non quibusdam.
+      </p>
+    </>
+  );
+}
