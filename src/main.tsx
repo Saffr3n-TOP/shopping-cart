ed84c17ec510a342c2f17f8ddf6111246595b050
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-01-23 20:02:02 +0300

    Rename main.sass to index.sass

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.tsx';
-import './styles/main.sass';
+import './styles/index.sass';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
