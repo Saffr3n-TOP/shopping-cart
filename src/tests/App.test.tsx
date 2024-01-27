0c8b94bd4060efc758b6f477216174012e6c4e48
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-01-27 14:43:15 +0300

    Add 2 tests for App component
    - test it rendering Home page on load
    - test it navigating correctly

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,6 +1,12 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { userEvent } from '@testing-library/user-event';
+import {
+  MemoryRouter,
+  createMemoryRouter,
+  RouterProvider
+} from 'react-router-dom';
+import { routes } from '../router';
 import App from '../App';
 
 describe('App component', () => {
@@ -12,5 +18,27 @@ describe('App component', () => {
   it('renders Main', () => {
     const app = render(<App />, { wrapper: MemoryRouter });
     expect(app.getByRole('main')).toBeDefined();
+  });
+
+  it('renders Home page', () => {
+    const app = render(<RouterProvider router={createMemoryRouter(routes)} />);
+
+    expect(app.getByRole('heading', { name: 'Home Page' })).toBeDefined();
+    expect(app.getAllByText(/lorem/i).length).toBe(2);
+  });
+
+  it('navigates correctly', async () => {
+    globalThis.fetch = vi
+      .fn()
+      .mockResolvedValue({ json: vi.fn().mockResolvedValue([]) });
+
+    const user = userEvent.setup();
+    const app = render(<RouterProvider router={createMemoryRouter(routes)} />);
+
+    await user.click(app.getByRole('link', { name: 'Shop' }));
+    expect(app.getByRole('heading', { name: 'Shop' })).toBeDefined();
+
+    await user.click(app.getByRole('link', { name: 'Cart' }));
+    expect(app.getByRole('heading', { name: 'Cart' })).toBeDefined();
   });
 });
